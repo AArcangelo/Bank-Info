ee201adca10c556821352d0b9ea586fc463f8d79
AArcangelo <98560442+AArcangelo@users.noreply.github.com>
2022-02-03 22:16:45 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+'''
+deposit 
+withdraw
+transfer
+stores accounts/create accounts
+login/password
+can't withdraw more than you have
+
+
+Account Info:
+Name(string)
+City(string)
+Email address(string)
+Social Security Number(string and integer)
+Phone Number(integer)
+
+Is checking(boolean)
+current balance(integer)
+
+'''

229ba5bed048a066ad30727424a63beb009962f7
AArcangelo <98560442+AArcangelo@users.noreply.github.com>
2022-02-10 22:37:35 +0000

    made accounts

--- main.py
+++ main.py
@@ -1,6 +1,40 @@
 from account import account
-myaccount = account("April","Rainbowpencil@gmail.com","123-456-789","California",1234567890,True)
-print(myaccount.phone_number)
+
+accounts = []
+def add_account():
+  nameInput = input("Enter your name")
+  cityInput = input("Enter your City")
+  emailInput = input("Enter your Email Address")
+  socialInput = input("Enter your Social Security Number")
+  numberInput = input("Enter your Phone Number")
+  is_checking = bool(input("Is this a Checking Account?"))
+  new_account = account(nameInput,cityInput,emailInput,socialInput,numberInput,is_checking)
+  accounts.append(new_account)
+  print(len(accounts))
+  return add_account()
+
+
+
+
+def mainMenu():
+  print("Welcome to April's ATM")
+  userChoice = input("A-Add Account,B-Load Account,C-Withdraw,D-Deposit,E-Check Balance")
+  if userChoice == "A":
+     return add_account()
+  elif userChoice == "B":
+    print("Load Account")
+  elif userChoice == "C":
+    print("Withdraw")
+  elif userChoice == "D":
+    print("Deposit")
+  elif userChoice == "E":
+    print("Check Balance")
+  else:
+    print("Error")
+    return mainMenu()
+  
+mainMenu()
+
 
 
 
